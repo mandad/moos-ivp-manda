32d3a2712ba295dcf017709fd3bbe23a4a14df5b
Damian Manda <damian9@gmail.com>
2015-08-08 13:14:55 -0400

    Adding new mendums mission

---
+++ missions/mendums_lake/launch_python.sh
@@ -0,0 +1,3 @@
+python ../../../asv-dev/utilities/python_moosapps/sonar_simulator.py &
+python ../../../asv-dev/utilities/python_moosapps/record_swath.py &
+python ../../../asv-dev/utilities/python_moosapps/path_plan.py &
