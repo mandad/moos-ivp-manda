9ac59f9d82421ec7472ed8ccd381f2b1eb4df713
Damian Manda <damian9@gmail.com>
2015-10-07 19:07:15 -0400

    Ocean Disc Day Sim Working

--- missions/2015_portsmouth_M1/launch.sh
+++ missions/2015_portsmouth_M1/launch.sh
@@ -25,8 +25,11 @@ done
 #-------------------------------------------------------
 VNAME1="boat1"      # The first   vehicle community
 VNAME2="boat2"       # The second  vehicle community
+VNAME_ASV="ASV"
 START_POS1="500,500"  
-START_POS2="760,-1180"  
+START_POS2="760,-1180"
+START_POS_ASV="0,0"
+
 
 # What is nsplug? Type "nsplug --help" or "nsplug --manual"
 
@@ -43,7 +46,11 @@ nsplug meta_vehicle.moos targ_$VNAME2.moos -f WARP=$TIME_WARP  VTYPE=SHIP \
    VNAME=$VNAME2      START_POS=$START_POS2  VLENGTH=15                  \
    VPORT="9002"       SHARE_LISTEN="9302"    
 
-nsplug meta_vehicle.bhv targ_$VNAME2.bhv -f VNAME=$VNAME2 START_POS=$START_POS2 
+nsplug meta_vehicle.bhv targ_$VNAME2.bhv -f VNAME=$VNAME2 START_POS=$START_POS2
+
+nsplug meta_asv.moos targ_asv.moos -f WARP=$TIME_WARP  VTYPE=SHIP \
+   VNAME=$VNAME_ASV     START_POS=$START_POS_ASV   VLENGTH=4             \
+   VPORT="9003"       SHARE_LISTEN="9303"  
 
 if [ ${JUST_MAKE} = "yes" ] ; then
     exit 0
@@ -54,10 +61,13 @@ fi
 #-------------------------------------------------------
 printf "Launching $VNAME1 MOOS Community (WARP=%s) \n" $TIME_WARP
 pAntler targ_$VNAME1.moos >& /dev/null &
-sleep .25
+sleep .5
 printf "Launching $VNAME2 MOOS Community (WARP=%s) \n" $TIME_WARP
 pAntler targ_$VNAME2.moos >& /dev/null &
-sleep .25
+sleep .5
+printf "Launching $VNAME_ASV MOOS Community (WARP=%s) \n" $TIME_WARP
+pAntler targ_asv.moos >& /dev/null &
+sleep .5
 printf "Launching $SNAME MOOS Community (WARP=%s) \n"  $TIME_WARP
 pAntler targ_shoreside.moos >& /dev/null &
 printf "Done \n"
@@ -65,7 +75,7 @@ printf "Done \n"
 uMAC targ_shoreside.moos
 
 printf "Killing all processes ... \n"
-kill %1 %2 %3 
+kill %1 %2 %3 %4
 printf "Done killing processes.   \n"
 
 
