550688890d78d611a7d2ba3bf4cd35c2515bc8ec
Damian Manda <damian.manda@noaa.gov>
2016-01-06 03:20:10 +0000

    Added mission for testing

--- missions/tj_zboat/generate_lines_mission.sh
+++ missions/tj_zboat/generate_lines_mission.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 #-------------------------------------------------------
 #  Part 1: Check for and handle command-line arguments
 #-------------------------------------------------------
@@ -7,25 +7,25 @@ JUST_MAKE="no"
 for ARGI; do
     if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ] ; then
 	printf "%s [SWITCHES] [time_warp]   \n" $0
-	printf "  --just_build, -j    \n" 
-	printf "  --help, -h         \n" 
+	printf "  --just_build, -j    \n"
+	printf "  --help, -h         \n"
 	exit 0;
-    elif [ "${ARGI//[^0-9]/}" = "$ARGI" -a "$TIME_WARP" = 1 ]; then 
+    elif [ "${ARGI//[^0-9]/}" = "$ARGI" -a "$TIME_WARP" = 1 ]; then
         TIME_WARP=$ARGI
     elif [ "${ARGI}" = "--just_build" -o "${ARGI}" = "-j" ] ; then
 	JUST_MAKE="yes"
-    else 
+    else
 	printf "Bad Argument: %s \n" $ARGI
 	exit 0
     fi
 done
 
 #-------------------------------------------------------
-#  Part 2: Create the .moos and .bhv files. 
+#  Part 2: Create the .moos and .bhv files.
 #-------------------------------------------------------
-SCENARIO=1
+SCENARIO=2
 # 1 = Chincoteague, VA
-# 2 = 
+# 2 = Test at home
 if [ $SCENARIO = 1 ]; then
   #MOOS Stuff
   START_POS="3000,8240"
@@ -45,7 +45,26 @@ if [ $SCENARIO = 1 ]; then
   SURVEY_SPEED=1.6
 fi
 
-# What is nsplug? Type "nsplug --help" or "nsplug --manual" 
+if [ $SCENARIO = 2 ]; then
+  #MOOS Stuff
+  START_POS="3000,8240"
+  LAT_ORIGIN=37.7499688
+  LONG_ORIGIN=-75.4585006
+  TIFF_FILE=data/Chincoteague_Ref.tif
+  PAN_X=0
+  PAN_Y=0
+  ZOOM=0.53
+  SHORE_IP="192.168.1.166"
+  SHORE_PORT=9300
+  # BOAT_MODEM_IP="166.150.166.136"
+  BOAT_MODEM_IP="192.168.1.243"
+  BOAT_PORT=9301
+  #Behavior
+  HOME_POS="3510,9080"
+  SURVEY_SPEED=1.6
+fi
+
+# What is nsplug? Type "nsplug --help" or "nsplug --manual"
 
 nsplug zboat_lines_remote.moos targ_zboat_lines_remote.moos -f WARP=$TIME_WARP \
    LAT_ORIGIN=$LAT_ORIGIN   LONG_ORIGIN=$LONG_ORIGIN   START_POS=$START_POS \
@@ -85,5 +104,3 @@ fi
 # printf "Killing all processes ... \n"
 # kill %1 %2 %3 %4
 # printf "Done killing processes.   \n"
-
-
