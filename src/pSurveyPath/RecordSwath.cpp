062328a38e9f058688d6f8cf08e1f45d4da94759
Damian Manda <damian9@gmail.com>
2016-02-24 19:59:37 -0500

    Port of RecordSwath done

--- src/pSurveyPath/RecordSwath.cpp
+++ src/pSurveyPath/RecordSwath.cpp
@@ -7,14 +7,16 @@
 
 #include "RecordSwath.h"
 #include <cmath>
+#include <algorithm>
 #include "AngleUtils.h"
+#include "GeomUtils.h"
 
 //---------------------------------------------------------
 // Constructor
 
 RecordSwath::RecordSwath(double interval) : m_min_allowable_swath(0),
                          m_has_records(false), m_acc_dist(0),
-                         m_interval(interval)
+                         m_interval(interval), m_output_side(BoatSide::Unknown)
 {
   /*
   // Define a precision model using 0,0 as the reference origin
@@ -37,4 +39,104 @@ RecordSwath::RecordSwath(double interval) : m_min_allowable_swath(0),
   m_interval_swath[BoatSide::Stbd] = std::vector<double>();
   m_outer_points[BoatSide::Port] = std::vector<geos::geom::Point*>();
   m_outer_points[BoatSide::Stbd] = std::vector<geos::geom::Point*>();
+}
+
+bool RecordSwath::AddRecord(double swath_stbd, double swath_port, double loc_x,
+                            double loc_y, double heading) {
+  SwathRecord record = {loc_x, loc_y, heading, swath_stbd, swath_port};
+  m_interval_record.push_back(record);
+  m_interval_swath[BoatSide::Stbd].push_back(swath_stbd);
+  m_interval_swath[BoatSide::Port].push_back(swath_port);
+
+  if (m_has_records) {
+    m_acc_dist += distPointToPoint(m_last_x, m_last_y, loc_x, loc_y);
+    if (m_acc_dist > m_interval) {
+      m_acc_dist = 0;
+      MinInterval();
+    }
+  }
+
+  // Add progressively to the coverage model
+  return AddToCoverage(record);
+
+  m_has_records = true;
+}
+
+bool RecordSwath::AddToCoverage(SwathRecord record) {
+  // Tackle this later
+  return true;
+}
+
+void RecordSwath::MinInterval() {
+  // Get the record from the side we are offsetting
+  std::vector<double>* side_record = &m_interval_swath[m_output_side];
+
+  int min_index = 0;
+  if (side_record->size() > 0) {
+    min_index = std::min_element(side_record->begin(), side_record->end())
+      - side_record->begin();
+  }
+
+  if (m_interval_record.size() > min_index) {
+    // Add the first point if this is the first interval in the record
+    if (m_min_record.size() == 0 && min_index != 0) {
+      m_min_record.push_back(m_interval_record[0]);
+    }
+    m_min_record.push_back(m_interval_record[min_index]);
+    // These are always cleared in the python version
+    m_interval_record.clear();
+    m_interval_swath.clear();
+  }
+}
+
+bool RecordSwath::SaveLast() {
+  if (m_min_record.size() > 0) {
+    SwathRecord last_min = m_min_record.back();
+    SwathRecord last_rec = m_interval_record.back();
+    if (last_min.loc_x != last_rec.loc_x && last_min.loc_y != last_rec.loc_y) {
+      m_min_record.push_back(last_rec);
+    }
+    return true;
+  }
+  return false;
+}
+
+void RecordSwath::ResetLine() {
+  m_interval_record.clear();
+  m_interval_swath[BoatSide::Stbd].clear();
+  m_interval_swath[BoatSide::Port].clear();
+  m_coverage = m_geom_factory->createPolygon();
+  m_outer_points[BoatSide::Stbd].clear();
+  m_outer_points[BoatSide::Port].clear();
+  m_acc_dist = 0;
+  m_has_records = false;
+}
+
+XYSegList RecordSwath::GetSwathOuterPts(BoatSide side) {
+  XYSegList points;
+  std::list<SwathRecord>::iterator record;
+  for (record = m_min_record.begin(); record != m_min_record.end(); record++) {
+    XYPoint outer_pt = GetOuterPoint(*record, m_output_side);
+    points.add_vertex(outer_pt);
+  }
+  return points;
+}
+
+XYPoint RecordSwath::GetOuterPoint(const SwathRecord &record, BoatSide side) {
+  // Could have SwathRecord be a class with functions to return representation
+  // as a vector or point.
+  double swath_width = 0;
+  double rotate_degs = 0;
+  if (side == BoatSide::Stbd) {
+    swath_width = record.swath_stbd;
+    rotate_degs = 90;
+  } else if (side == BoatSide::Port) {
+    swath_width = record.swath_port;
+    rotate_degs = -90;
+  }
+  XYVector swath_vector(record.loc_x, record.loc_y, swath_width,
+    record.heading);
+  swath_vector.augAngle(rotate_degs);
+  return XYPoint(swath_vector.xpos() + swath_vector.xdot(),
+    swath_vector.ypos() + swath_vector.ydot());
 }
--- src/pSurveyPath/RecordSwath.h
+++ src/pSurveyPath/RecordSwath.h
@@ -21,16 +21,27 @@
 //MOOS Headers
 #include "XYPoint.h"
 #include "XYSegList.h"
+#include "XYVector.h"
 
 #include <list>
 #include <vector>
 #include <map>
 
+/**
+ * @enum BoatSide
+ * @brief Indicates the side of a boat for the swath
+ */
 enum class BoatSide {
   Stbd,
-  Port
+  Port,
+  Unknown
 };
 
+/**
+ * @class RecordSwath
+ * @brief Records points of a sonar swath for analysis of the coverage and
+ * subsequent tracks.
+ */
 class RecordSwath
 {
 private:
@@ -38,6 +49,7 @@ private:
     double loc_x;
     double loc_y;
     double heading;
+    // Could make this a map w/ BoatSide index
     double swath_stbd;
     double swath_port;
   };
@@ -45,19 +57,58 @@ private:
  public:
    RecordSwath(double interval = 10);
    ~RecordSwath() {};
+   /**
+    * Adds a recorded swath to the path.
+    * @param  swath_stbd Swath width to starboard
+    * @param  swath_port Swath width to port
+    * @param  loc_x      X coordinate of position where record takes place
+    * @param  loc_y      Y coordinate of position
+    * @param  heading    Heading of the vessel at the time of recording
+    * @return            True if the record coverage was successfully added
+    */
    bool AddRecord(double swath_stbd, double swath_port, double loc_x, double loc_y,
           double heading);
+   /**
+    * Resets the storage for a new line.
+    */
    void ResetLine();
-   void SaveLast();
-   void GetSwathOuterPts(BoatSide side, XYSegList& points);
-   bool GetSwathCoverage(BoatSide side, geos::geom::Polygon& coverage);
+   /**
+    * Saves the last point to a record.
+    * This makes sure that the last swath (after crossing the boundary) is
+    * recorded so that it is included in planning.
+    * @return If the min_record is valid
+    */
+   bool SaveLast();
+   /**
+    * [GetSwathOuterPts description]
+    * @param side   [description]
+    * @param points [description]
+    */
+   XYSegList GetSwathOuterPts(BoatSide side);
+   bool GetSwathCoverage(BoatSide side, geos::geom::Polygon &coverage);
    double GetSwathWidth(BoatSide side, unsigned int index);
    std::vector<double> GetAllSwathWidths(BoatSide side);
    XYPoint GetSwathLocation(unsigned int index);
+   /**
+    * Sets the side that will be used for outer point determination
+    * @param side Side of the boat on which to generate outer swath points
+    */
+   void SetOutputSide(BoatSide side) { m_output_side = side; }
 
  protected:
+   /**
+    * Determines the minimum swath over the recorded interval and places it into
+    * a list of minimums.
+    */
    void MinInterval();
-   XYPoint GetOuterPoint(SwathRecord record, BoatSide side);
+   /**
+    * Gets the x,y position of the edge of a swath from a record
+    * @param  record The swath record to use for location and width
+    * @param  side   The side of the boat on which to project the swath
+    * @return        Location of the swath outer points
+    */
+   XYPoint GetOuterPoint(const SwathRecord &record, BoatSide side);
+   bool AddToCoverage(SwathRecord record);
 
  private:
    // Configuration Variables
@@ -65,12 +116,15 @@ private:
 
    // State varables
    std::vector<SwathRecord> m_interval_record;
+   std::list<SwathRecord> m_min_record;
    std::map<BoatSide, std::vector<double>> m_interval_swath;
    double m_last_x;
    double m_last_y;
    bool m_has_records;
    double m_acc_dist;
    double m_interval;
+   SwathRecord m_previous_record;
+   BoatSide m_output_side;
 
    // Geometry Stuff
    std::map<BoatSide, std::vector<geos::geom::Point*>> m_outer_points;
